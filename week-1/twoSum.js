398c09f1be051ca3dbfa331c7b694f3351fb506f
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-05-05 00:01:06 -0600

    Solved twoSum

---
+++ week-1/twoSum.js
@@ -0,0 +1,43 @@
+"use strict";
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+var twoSum = function (numList, target) {
+    var numObject = {};
+    for (var i = 0; i < numList.length; i++) {
+        if (numObject.hasOwnProperty(numList[i])) {
+            console.log(numObject);
+            if (numObject[numList[i]][1] + numList[i] === target) {
+                return [i, numObject[numList[i]][0]];
+            }
+        }
+        else {
+            if (target <= numList[i]) {
+                numObject[numList[i] - target] = [i, numList[i] - (numList[i] - target)];
+            }
+            else {
+                numObject[target - numList[i]] = [i, target - (target - numList[i])];
+            }
+        }
+    }
+    return [-1, -1];
+};
+exports.default = twoSum;
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
