23c5749452b25c7427d6f09d1a769f7df4066554
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-07-12 17:59:00 -0600

    Solved encode and decode strings

---
+++ neetcode/1_arrays_and_hashing/8_encode-and-decode-strings/main.py
@@ -0,0 +1,27 @@
+# Design an algorithm to encode a list of strings to a string.
+# The encoded string is then sent over the network and is decoded back to the original list of strings.
+# Please implement encode and decode
+
+# Example 1:
+# Input: ["lint", "code","love","you"]
+# Output: ["lint", "code","love","you"]
+# Explanation:
+# One possible encode method is: "lint:;code:;love:;you:;"
+
+
+def encode(strs):
+    res = ""
+    for string in strs:
+        res += str(len(string)) + "#" + string
+    return res
+
+def decode(str):
+    res, i = [], 0
+    while i < len(str):
+        j = i
+        while str[j] != '#':
+            j += 1
+        length = int(str[i:j])
+        res.append(str[j+1:j+1+length])
+        i = j + 1 + length
+    return res
